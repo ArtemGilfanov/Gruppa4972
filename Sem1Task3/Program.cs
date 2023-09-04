fea1014f691fc271f69fd74faafa968841044910
Artem_Gilfanov <a.gilfanovv@yandex.ru>
2023-09-04 23:47:05 +0300

    Seminar 1

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+int day = int.Parse(Console.ReadLine() ?? "0");
+
+string[] dayOfWeek = new string[7];
+
+dayOfWeek[0] = "Понедельник";
+dayOfWeek[1] = "Вторник";
+dayOfWeek[2] = "Среда";
+dayOfWeek[3] = "Четверг";
+dayOfWeek[4] = "Пятница";
+dayOfWeek[5] = "Суббота";
+dayOfWeek[6] = "Воскресенье";
+
+Console.WriteLine(dayOfWeek[day - 1]);
+
+// string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum));
