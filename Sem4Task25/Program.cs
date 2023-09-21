1688c99c49317804823ed8a0cf72d7c988532090
Artem_Gilfanov <a.gilfanovv@yandex.ru>
2023-09-21 20:18:20 +0300

    Add Homework Sem4

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Введите число - ");
+int exponent = Prompt("Введите степень для первого числа - ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} это {Power(powerBase, exponent)}");
+}
